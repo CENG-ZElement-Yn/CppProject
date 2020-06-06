87711c40f7b92991de76027772500b9a0e82cee9
CENG-ZElement-Yn <54050294+CENG-ZElement-Yn@users.noreply.github.com>
2020-06-06 14:26:00 +0800

    Update DOSRecollection.cpp

--- DOSRecollection.cpp
+++ DOSRecollection.cpp
@@ -2,13 +2,21 @@
 #include<windows.h>
 using namespace std;
 int number;
+
 void JustSafe(){
   
+  
+}
+void DirtyDirty(){
+
+}
+void BadBoy(){
+
 }
 void start(){
     cout<<"Welocme to 'Some Lost'Game"<<endl;
     cout<<"Now Please select a mode"<<endl;
-    cout<<"1. Just Safe 2. dity! dity 3.bad boy"<<endl;
+    cout<<"1. Just Safe 2. dirty! dirty 3.bad boy"<<endl;
     cout<<">";
     cin>>number;
     if(number==1){
